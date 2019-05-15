9d619dd760a2159457251ae592d6529fd70ef029
achiengcindy <achiengcindy36@gmail.com>
2019-05-15 15:03:43 +0300

    js script

---
+++ 5.js
@@ -0,0 +1,9 @@
+var a = [];  // create a variable a and assign it an empty array
+var b = [];   // create a variable b and assign it an empty array
+for (i = 0; i<=10; i++)
+{
+    a[i]= Math.random();
+    b[i]= Math.random();
+}
+console.log(a);
+console.log(a);
