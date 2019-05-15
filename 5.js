ed62284ef5d101346bd13e0d14f15e9ef86441b5
Kabue Charles <mckabue@gmail.com>
2019-05-15 15:19:47 +0300

    Format

--- 5.js
+++ 5.js
@@ -11,18 +11,17 @@ var b = [];
 /**
  * Loop from 0 to 10
  */
-for (i = 0; i<=10; i++)
-{
+for (i = 0; i <= 10; i++) {
     /**
      * Assign a random value to the current index 
      * (which is the value of variable i) to array a
      */
-    a[i]= Math.random();
+    a[i] = Math.random();
     /**
      * Assign a random value to the current index 
      * (which is the value of variable i) to array b
      */
-    b[i]= Math.random();
+    b[i] = Math.random();
 }
 
 /**
