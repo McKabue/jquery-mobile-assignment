6fc32f5f4e58bf4ae51305ea504a57839fcd194e
Kabue Charles <mckabue@gmail.com>
2019-05-15 15:17:44 +0300

    JS Doc

--- 5.js
+++ 5.js
@@ -1,9 +1,36 @@
-var a = [];  // create a variable a and assign it an empty array
-var b = [];   // create a variable b and assign it an empty array
+/**
+ * Declare a variable named a and assign it an empty array
+ */
+var a = [];
+
+/**
+ * Declare a variable named b and assign it an empty array
+ */
+var b = [];
+
+/**
+ * Loop from 0 to 10
+ */
 for (i = 0; i<=10; i++)
 {
+    /**
+     * Assign a random value to the current index 
+     * (which is the value of variable i) to array a
+     */
     a[i]= Math.random();
+    /**
+     * Assign a random value to the current index 
+     * (which is the value of variable i) to array b
+     */
     b[i]= Math.random();
 }
+
+/**
+ * Print the value of array a
+ */
 console.log(a);
-console.log(a);
+
+/**
+ * Print the value of array b
+ */
+console.log(b);
